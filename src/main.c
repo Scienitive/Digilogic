a658e2b66cc40c89039b99a8950eb1528b61b2bd
Scienitive <scienitive77@gmail.com>
2023-12-23 19:30:41 +0100

    main made better

--- src/main.c
+++ src/main.c
@@ -2,19 +2,34 @@
 #include "../include/scene/scene.h"
 #include <raylib.h>
 
-int main(void) {
-	Scene app;
-	app.pos.x = 0;
-	app.pos.y = 0;
-	Controls controls = {false, {GetMouseX(), GetMouseY()}};
-	InitWindow(800, 450, "raylib [core] example - basic window");
+void update(Scene *app, Controls *controls) {
+	input_handler(app, controls);
+}
 
+void draw(Scene *app) {
+	BeginDrawing();
+	ClearBackground(RAYWHITE);
+	draw_scene(app);
+	EndDrawing();
+}
+
+int main(void) {
+	// Initial
+	const int SCREEN_WIDTH = 800;
+	const int SCREEN_HEIGHT = 450;
+	SetTargetFPS(144);
+	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "raylib [core] example - basic window");
+	SetWindowMinSize(800, 450);
+
+	// General Structs
+	Scene app = {{0, 0}};
+	Controls controls = {false, GetMousePosition()};
+
+	// The Loop
 	while (!WindowShouldClose()) {
-		input_handler(&app, &controls);
-		BeginDrawing();
-		ClearBackground(RAYWHITE);
-		draw_scene(&app);
-		EndDrawing();
+		update(&app, &controls);
+		draw(&app);
 	}
 
 	CloseWindow();
